cb47fba353a446e0434329009307ddd10210c0c2
nickbauman <nick.bauman@cirrusmachina.com>
2015-08-02 10:52:49 -0500

    sequential, synchronous  access of server from client

--- async_golang/src/bench/client.go
+++ async_golang/src/bench/client.go
@@ -8,24 +8,28 @@ import (
 )
 
 func RunClient(host_and_port string, num_iterations int64) {
-	//results := make(map[int]int)
+	results := make(map[int]int)
   var buffer bytes.Buffer
+
   buffer.WriteString("http://")
   buffer.WriteString(host_and_port)
   url := buffer.String()
 
   fmt.Printf("%s\n", url)
 
-	resp, err := http.Get(url)
-  fmt.Printf("err: %+v\n", err)
-  fmt.Printf("resp: %+v\n", resp)
-
-	if err != nil {
-		// do something
-    fmt.Printf("error: %+v", err)
+  for i := int64(0); i<num_iterations; i++ {
+		resp, _ := http.Get(url)
+    //fmt.Printf("err: %+v\n", err)
+    //fmt.Printf("resp: %+v\n", resp)
+    val, ok := results[resp.StatusCode]
+    if(ok) {
+      results[resp.StatusCode] = val + 1
+    } else {
+      results[resp.StatusCode] = 1
+    }
+	  defer resp.Body.Close()
+	  ioutil.ReadAll(resp.Body)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-  fmt.Printf("err: %+v\n", err)
-  fmt.Printf("body: '%s'\n", body)
+
+  fmt.Printf("results: %+v\n", results)
 }
